8a7dabcac5e5d1b2ae177800263818b83e993281
Filipe Siqueira <filipedev040990@gmail.com>
2023-02-09 17:07:13 -0300

    feat: implements facebook authentication interface

---
+++ src/domain/features/facebook-authentication.ts
@@ -0,0 +1,13 @@
+import { AccessToken } from '@/domain/types'
+import { AuthenticationError } from '@/domain/errors'
+
+export interface FacebookAuthentication {
+  execute: (input: FacebookAuthentication.Input) => Promise<FacebookAuthentication.Output>
+}
+
+namespace FacebookAuthentication {
+  export type Input = {
+    accessToken: string
+  }
+  export type Output = AccessToken | AuthenticationError
+}
