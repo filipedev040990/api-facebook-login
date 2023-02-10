fcb70b15297819fcac0ea3f0878f1baa9448c476
Filipe Siqueira <filipedev040990@gmail.com>
2023-02-10 10:55:38 -0300

    feat: ensure not update name if its not empty

--- src/application/services/facebook-authentication.ts
+++ src/application/services/facebook-authentication.ts
@@ -2,6 +2,7 @@ import { GetFacebookUserApi } from '@/application/contracts/apis'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { SaveUserFromFacebookRepository, GetUserRepository } from '@/application/repositories'
+import { FacebookUserEntity } from '@/domain/entities/facebook-user'
 
 export class FacebookAuthenticationService {
   constructor (
@@ -13,13 +14,8 @@ export class FacebookAuthenticationService {
     const facebookData = await this.facebookApi.getUser({ token: params.token })
     if (facebookData) {
       const userData = await this.userRepository.getByEmail({ email: facebookData.email })
-
-      await this.userRepository.saveWithFacebook({
-        id: userData?.id,
-        name: userData?.name ?? facebookData.name,
-        email: facebookData.email,
-        facebookId: facebookData.facebookId
-      })
+      const facebookUser = new FacebookUserEntity(facebookData, userData)
+      await this.userRepository.saveWithFacebook(facebookUser)
     }
 
     return new AuthenticationError()
--- tests/entities/facebook-user.spec.ts
+++ tests/entities/facebook-user.spec.ts
@@ -1,28 +1,46 @@
 import { FacebookData, FacebookUserEntity, UserData } from '@/domain/entities/facebook-user'
 
-const facebookData: FacebookData = {
-  email: 'anyEmail@email.com',
-  name: 'Any Facebook Name',
-  facebookId: 'Any Facebook Id'
-}
-
-const userData: UserData = {
-  id: 'Any User Id',
-  name: 'Any User Name'
-}
+let facebookData: FacebookData
+let userData: UserData
 
 describe('FacebookUserEntity', () => {
+  beforeEach(() => {
+    facebookData = {
+      email: 'anyEmail@email.com',
+      name: 'Any Facebook Name',
+      facebookId: 'Any Facebook Id'
+    }
+
+    userData = {
+      id: 'Any User Id',
+      name: 'Any User Name'
+    }
+  })
   test('should create a new instance of FacebookUserEntity with facebookData only', () => {
     const sut = new FacebookUserEntity(facebookData)
+
     expect(sut).toEqual(facebookData)
   })
 
   test('should update name if its empty', () => {
     userData.name = undefined
+
     const sut = new FacebookUserEntity(facebookData, userData)
+
     expect(sut).toEqual({
       id: 'Any User Id',
       name: 'Any Facebook Name',
+      email: 'anyEmail@email.com',
+      facebookId: 'Any Facebook Id'
+    })
+  })
+
+  test('should not update name if its not empty', () => {
+    const sut = new FacebookUserEntity(facebookData, userData)
+
+    expect(sut).toEqual({
+      id: 'Any User Id',
+      name: 'Any User Name',
       email: 'anyEmail@email.com',
       facebookId: 'Any Facebook Id'
     })
