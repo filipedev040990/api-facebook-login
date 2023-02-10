116da74a4a4dae0c16888ce7f503f9e6c6c4ac6b
Filipe Siqueira <filipedev040990@gmail.com>
2023-02-10 10:52:59 -0300

    feat: should update name if its empty

--- tests/entities/facebook-user.spec.ts
+++ tests/entities/facebook-user.spec.ts
@@ -1,4 +1,4 @@
-import { FacebookData, FacebookUserEntity } from '@/domain/entities/facebook-user'
+import { FacebookData, FacebookUserEntity, UserData } from '@/domain/entities/facebook-user'
 
 const facebookData: FacebookData = {
   email: 'anyEmail@email.com',
@@ -6,9 +6,25 @@ const facebookData: FacebookData = {
   facebookId: 'Any Facebook Id'
 }
 
+const userData: UserData = {
+  id: 'Any User Id',
+  name: 'Any User Name'
+}
+
 describe('FacebookUserEntity', () => {
   test('should create a new instance of FacebookUserEntity with facebookData only', () => {
     const sut = new FacebookUserEntity(facebookData)
     expect(sut).toEqual(facebookData)
+  })
+
+  test('should update name if its empty', () => {
+    userData.name = undefined
+    const sut = new FacebookUserEntity(facebookData, userData)
+    expect(sut).toEqual({
+      id: 'Any User Id',
+      name: 'Any Facebook Name',
+      email: 'anyEmail@email.com',
+      facebookId: 'Any Facebook Id'
+    })
   })
 })
