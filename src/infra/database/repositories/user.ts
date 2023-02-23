fb39b92cc123afcd728b1d99e9dcfb90aed2e73c
Filipe Siqueira <filipedev040990@gmail.com>
2023-02-23 18:49:35 -0300

    refactor: move TypeORM getRepository method to functions

--- src/infra/database/repositories/user.ts
+++ src/infra/database/repositories/user.ts
@@ -3,10 +3,9 @@ import { User } from '@/infra/database/entities'
 import { getRepository } from 'typeorm'
 
 export class UserRepository implements GetUserRepository, SaveUserFromFacebookRepository {
-  private readonly repository = getRepository(User)
-
   async getByEmail ({ email }: GetUserRepository.Input): Promise<GetUserRepository.Output> {
-    const user = await this.repository.findOne({ email })
+    const repository = getRepository(User)
+    const user = await repository.findOne({ email })
     if (user) {
       return {
         id: user.id.toString(),
@@ -16,14 +15,15 @@ export class UserRepository implements GetUserRepository, SaveUserFromFacebookRe
   }
 
   async saveWithFacebook ({ id, name, email, facebookId }: SaveUserFromFacebookRepository.Input): Promise<SaveUserFromFacebookRepository.Output> {
+    const repository = getRepository(User)
     let resultId: string
 
     if (!id) {
-      const user = await this.repository.save({ name, email, facebookId })
+      const user = await repository.save({ name, email, facebookId })
       resultId = user.id.toString()
     } else {
       resultId = id
-      await this.repository.update({ id: +id }, { name, facebookId })
+      await repository.update({ id: +id }, { name, facebookId })
     }
 
     return { id: resultId }
