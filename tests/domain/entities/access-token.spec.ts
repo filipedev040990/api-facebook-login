71781bc0a9410a3a14ece52e7e0ece0ba36466f0
Filipe Siqueira <filipedev040990@gmail.com>
2023-02-26 08:30:56 -0300

    refactor: change AccessToken class to constant

--- tests/domain/entities/access-token.spec.ts
+++ tests/domain/entities/access-token.spec.ts
@@ -1,12 +1,6 @@
 import { AccessToken } from '@/domain/entities'
 
 describe('AccessToken', () => {
-  test('should create a new instance of AccessToken', () => {
-    const sut = new AccessToken('any_value')
-
-    expect(sut).toEqual({ value: 'any_value' })
-  })
-
   test('should expire in 1800000 ms', () => {
     expect(AccessToken.expirationInMs).toBe(1800000)
   })
