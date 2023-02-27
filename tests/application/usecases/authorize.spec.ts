595b1a66329cc7e2261ac05fd438390decce5616
Filipe Siqueira <filipedev040990@gmail.com>
2023-02-27 08:24:31 -0300

    feat: ensure Authorize return correct data on success

--- tests/application/usecases/authorize.spec.ts
+++ tests/application/usecases/authorize.spec.ts
@@ -1,18 +1,19 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
 export interface TokenValidator {
-  validateToken: (input: TokenValidator.Input) => Promise<void>
+  validateToken: (input: TokenValidator.Input) => Promise<TokenValidator.Output>
 }
 
 namespace TokenValidator {
   export type Input = {token: string}
+  export type Output = { key: string }
 }
 
 export class Authorize {
   constructor (private readonly crypto: TokenValidator) {}
 
-  async execute (input: TokenValidator.Input): Promise<void> {
-    await this.crypto.validateToken(input)
+  async execute (input: TokenValidator.Input): Promise<TokenValidator.Output> {
+    return this.crypto.validateToken(input)
   }
 }
 
@@ -24,6 +25,7 @@ describe('Authorize', () => {
   beforeAll(() => {
     token = 'any_token'
     crypto = mock()
+    crypto.validateToken.mockResolvedValue({ key: 'any_key' })
   })
 
   beforeEach(() => {
@@ -35,5 +37,11 @@ describe('Authorize', () => {
 
     expect(crypto.validateToken).toHaveBeenCalledTimes(1)
     expect(crypto.validateToken).toHaveBeenCalledWith({ token })
+  })
+
+  test('should return correct data on success', async () => {
+    const response = await sut.execute({ token })
+
+    expect(response).toEqual({ key: 'any_key' })
   })
 })
