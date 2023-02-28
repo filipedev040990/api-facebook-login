eebb1dd01e2f35c963524c42494b79b8bc727ace
Filipe Siqueira <filipedev040990@gmail.com>
2023-02-28 18:43:21 -0300

    refactor: move production code to its own file

---
+++ src/infra/adapters/middlewares/authentication.ts
@@ -0,0 +1,28 @@
+import { forbidden, successRequest } from '@/application/shared/helpers/http'
+import { HttpResponse } from '@/application/shared/types'
+import { Authorize } from '@/application/usecases'
+import { RequiredStringValidator } from '@/infra/adapters/validation'
+
+type Input = { authorization: string }
+type Output = Error | { userId: string}
+
+export class AuthenticationMiddleware {
+  constructor (private readonly authorize: Authorize) {}
+  async execute ({ authorization }: Input): Promise<HttpResponse<Output>> {
+    if (!this.validate({ authorization })) {
+      return forbidden()
+    }
+
+    try {
+      const response = await this.authorize.execute({ token: authorization })
+      return successRequest({ userId: response.key })
+    } catch {
+      return forbidden()
+    }
+  }
+
+  private validate ({ authorization }: Input): boolean {
+    const error = new RequiredStringValidator(authorization, 'authorization').execute()
+    return error === undefined
+  }
+}
--- tests/infra/adapters/middlewares/authentication.spec.ts
+++ tests/infra/adapters/middlewares/authentication.spec.ts
@@ -1,33 +1,8 @@
 import { HttpResponse } from '@/application/shared/types'
-import { forbidden, successRequest } from '@/application/shared/helpers/http'
 import { ForbiddenError } from '@/application/shared/errors'
 import { Authorize } from '@/application/usecases'
 import { mock, MockProxy } from 'jest-mock-extended'
-import { RequiredStringValidator } from '@/infra/adapters/validation'
-
-export type Input = { authorization: string }
-export type Output = Error | { userId: string}
-
-export class AuthenticationMiddleware {
-  constructor (private readonly authorize: Authorize) {}
-  async execute ({ authorization }: Input): Promise<HttpResponse<Output>> {
-    if (!this.validate({ authorization })) {
-      return forbidden()
-    }
-
-    try {
-      const response = await this.authorize.execute({ token: authorization })
-      return successRequest({ userId: response.key })
-    } catch {
-      return forbidden()
-    }
-  }
-
-  private validate ({ authorization }: Input): boolean {
-    const error = new RequiredStringValidator(authorization, 'authorization').execute()
-    return error === undefined
-  }
-}
+import { AuthenticationMiddleware } from '@/infra/adapters/middlewares/authentication'
 
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware
