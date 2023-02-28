8d6f2cdc8d6c88e859dd2e123ebd79c885c4aef2
Filipe Siqueira <filipedev040990@gmail.com>
2023-02-28 18:20:49 -0300

    feat: ensure AuthenticationMiddleware call authorize once and with correct values

--- tests/infra/adapters/middlewares/authentication.spec.ts
+++ tests/infra/adapters/middlewares/authentication.spec.ts
@@ -1,22 +1,33 @@
 import { HttpResponse } from '@/application/shared/types'
 import { forbidden } from '@/application/shared/helpers/http'
 import { ForbiddenError } from '@/application/shared/errors'
+import { Authorize } from '@/application/usecases'
+import { mock, MockProxy } from 'jest-mock-extended'
+import { RequiredStringValidator } from '@/infra/adapters/validation'
 
 export type Input = { authorization: string }
 export type Output = Error
 
 export class AuthenticationMiddleware {
-  async execute (input: Input): Promise<HttpResponse<Output>> {
-    return forbidden()
+  constructor (private readonly authorize: Authorize) {}
+  async execute ({ authorization }: Input): Promise<HttpResponse<Output> | undefined> {
+    const error = new RequiredStringValidator(authorization, 'authorization').execute()
+    if (error) {
+      return forbidden()
+    }
+
+    await this.authorize.execute({ token: authorization })
   }
 }
 
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware
   let forbidenError: HttpResponse
+  let authorize: MockProxy<Authorize>
 
   beforeAll(() => {
-    sut = new AuthenticationMiddleware()
+    authorize = mock()
+    sut = new AuthenticationMiddleware(authorize)
     forbidenError = {
       statusCode: 403,
       body: new ForbiddenError()
@@ -39,5 +50,12 @@ describe('AuthenticationMiddleware', () => {
     const response = await sut.execute({ authorization: undefined as any })
 
     expect(response).toEqual(forbidenError)
+  })
+
+  test('should call authorize once and with correct values', async () => {
+    await sut.execute({ authorization: 'any_token' })
+
+    expect(authorize.execute).toHaveBeenCalledTimes(1)
+    expect(authorize.execute).toHaveBeenCalledWith({ token: 'any_token' })
   })
 })
