7f950c4a4c141253eb7b283bd14bbb55ac814b65
Filipe Siqueira <filipedev040990@gmail.com>
2023-03-01 17:24:34 -0300

    feat: ensure expressAdapterMiddleware respond with 403 and correct error

--- tests/infra/adapters/middlewares/express-middleware.spec.ts
+++ tests/infra/adapters/middlewares/express-middleware.spec.ts
@@ -3,6 +3,7 @@ import { mock, MockProxy } from 'jest-mock-extended'
 import { getMockReq, getMockRes } from '@jest-mock/express'
 import { expressAdapterMiddleware } from '@/infra/adapters/middlewares/express-middleware-adapter'
 import { AuthenticationMiddleware } from '@/infra/adapters/middlewares/authentication'
+import { ForbiddenError } from '@/application/shared/errors'
 
 describe('expressAdapterMiddleware', () => {
   let req: Request
@@ -28,5 +29,18 @@ describe('expressAdapterMiddleware', () => {
 
     expect(middleware.execute).toHaveBeenCalledTimes(1)
     expect(middleware.execute).toHaveBeenCalledWith(req.headers)
+  })
+
+  test('should respond with 403 and correct error', async () => {
+    middleware.execute.mockResolvedValueOnce({
+      statusCode: 403,
+      body: new ForbiddenError()
+    })
+    await sut(req, res, next)
+
+    expect(res.status).toHaveBeenCalledWith(403)
+    expect(res.status).toHaveBeenCalledTimes(1)
+    expect(res.json).toHaveBeenCalledWith({ error: new ForbiddenError() })
+    expect(res.json).toHaveBeenCalledTimes(1)
   })
 })
