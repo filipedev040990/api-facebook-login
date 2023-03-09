00eb23901f02239ac6bd47510f28d1e797d77d99
Filipe Siqueira <filipedev040990@gmail.com>
2023-03-09 18:16:40 -0300

    refactor: isolate SUT creation

--- tests/infra/adapters/crypto/uuid-handler.spec.ts
+++ tests/infra/adapters/crypto/uuid-handler.spec.ts
@@ -6,18 +6,21 @@ import { mocked } from 'ts-jest/utils'
 jest.mock('uuid')
 
 describe('UUIDHandler', () => {
-  test('should call uuid.v4', () => {
-    const sut = new UUIDHandler()
+  let sut: UUIDHandler
 
+  beforeAll(() => {
+    mocked(v4).mockReturnValue('anyUUID')
+  })
+  beforeEach(() => {
+    sut = new UUIDHandler()
+  })
+  test('should call uuid.v4', () => {
     sut.uuid({ key: 'anyKey' })
 
     expect(v4).toHaveBeenCalledTimes(1)
   })
 
   test('should return correct value', () => {
-    mocked(v4).mockReturnValueOnce('anyUUID')
-    const sut = new UUIDHandler()
-
     const uuid = sut.uuid({ key: 'anyKey' })
 
     expect(uuid).toBe('anyKey_anyUUID')
